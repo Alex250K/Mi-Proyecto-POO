796e2cb6d0ba981161227136a653102c380de6ff
PND-JorgeMarin <jorgemarin31931@correo.itm.edu.co>
2025-09-13 10:32:46 -0500

    Add files via upload

---
+++ Producto.cs
@@ -0,0 +1,16 @@
+﻿namespace InventarioLibreria.Models
+{
+    public abstract class Producto
+    {
+        public string Titulo { get; set; }
+        public decimal Precio { get; set; }
+
+        public Producto(string titulo, decimal precio)
+        {
+            Titulo = titulo;
+            Precio = precio;
+        }
+
+        public abstract void MostrarInfo();
+    }
+}
