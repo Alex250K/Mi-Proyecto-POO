c2f811768dfec47ba11dbc18e4c15fab49e2ed90
PND-JorgeMarin <jorgemarin31931@correo.itm.edu.co>
2025-09-13 10:33:39 -0500

    Add files via upload

---
+++ Venta.cs
@@ -0,0 +1,21 @@
+﻿namespace InventarioLibreria.Models
+{
+    public class Venta
+    {
+        public Libro LibroVendido { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Venta(Libro libro, int cantidad)
+        {
+            LibroVendido = libro;
+            Cantidad = cantidad;
+            Fecha = DateTime.Now;
+        }
+
+        public void MostrarVenta()
+        {
+            Console.WriteLine($"{Fecha:dd/MM/yyyy HH:mm} | {LibroVendido.Titulo} | Cantidad: {Cantidad} | Total: {(LibroVendido.Precio * Cantidad):C}");
+        }
+    }
+}
