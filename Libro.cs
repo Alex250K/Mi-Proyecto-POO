406d6c025381f0dfcbe93d71cf3107862e14d4cf
PND-JorgeMarin <jorgemarin31931@correo.itm.edu.co>
2025-09-13 10:30:44 -0500

    Add files via upload

---
+++ Libro.cs
@@ -0,0 +1,28 @@
+﻿namespace InventarioLibreria.Models
+{
+    public class Libro : Producto
+    {
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        private int stock;
+
+        public int Stock
+        {
+            get => stock;
+            set => stock = value >= 0 ? value : 0;
+        }
+
+        public Libro(string titulo, string autor, string isbn, decimal precio, int stock)
+            : base(titulo, precio)
+        {
+            Autor = autor;
+            ISBN = isbn;
+            Stock = stock;
+        }
+
+        public override void MostrarInfo()
+        {
+            Console.WriteLine($"{Titulo} - {Autor} | ISBN: {ISBN} | Stock: {Stock} | Precio: {Precio:C}");
+        }
+    }
+}
